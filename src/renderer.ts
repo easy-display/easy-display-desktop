992b7413fe21a8682361c680e2715f6e7e422987
Mohammed O. Tillawy <tillawy@gmail.com>
2018-08-03 14:59:03 +0300

    create the qr code window

--- src/renderer.ts
+++ src/renderer.ts
@@ -10,24 +10,26 @@ import {IMessage} from "./types";
 
 import axios, {AxiosResponse} from "axios";
 import electron = require("electron");
-const BrowserWindow = electron.remote.BrowserWindow;
+const ElectronBrowserWindow = electron.remote.BrowserWindow;
 import * as path from "path";
+import BrowserWindow = Electron.BrowserWindow;
 
 const isDevel = (): boolean => {
     return process.env.NODE_ENV === "development";
 };
 
-const openQrCodeDialogue = (proto: string, host: string, token: string ): void => {
-    const modalPath = path.join(`file://${__dirname}/qr.html?protocol=${proto}&host=${host}&token=${token}`);
+let qrWin: BrowserWindow;
+const openQrCodeDialogue = (scheme: string, host: string , version: string, token: string ): void => {
+    const modalPath = path.join(`file://${__dirname}/qr.html?scheme=${scheme}&version=${version}&host=${host}&token=${token}`);
     console.log(`modalPath: ${modalPath}`);
-    let win = new BrowserWindow({
+    qrWin = new ElectronBrowserWindow({
         alwaysOnTop: true,
-        height: 200 ,
+        height: 300 ,
         width: 300,
     });
-    win.on("close", () => { win = null; });
-    win.loadURL(modalPath);
-    win.show();
+    qrWin.on("close", () => { qrWin = null; });
+    qrWin.loadURL(modalPath);
+    qrWin.show();
 };
 
 const connectionUrl = (): string => {
@@ -40,16 +42,13 @@ const connectionUrl = (): string => {
 
 let socket: Socket;
 
-const connectSocket = (protocol: string, host: string, token: string ): void => {
+const connectSocket = (scheme: string, host: string, version: string , token: string ): void => {
     console.log("connectSocket ...");
-    // const token = "Az_678987";
-    // const host = "localhost:8999";
-    // const protocol = "http";
-    const uri = `${protocol}://${host}/desktop/0.1?client_type=desktop&token=${token}`;
+    const uri = `${scheme}://${host}/desktop/${version}?client_type=desktop&token=${token}`;
     socket = io.connect(uri);
     socket.on("connect", () => {
         console.log(`connect to server: ${uri} SUCCESS, socket.connected: ${socket.connected}`);
-        openQrCodeDialogue(protocol, host, token);
+        openQrCodeDialogue(scheme, host , version, token);
         // s.emit("event_to_client", { hello: "world" });
         socket.on("event_server_to_desktop", (data: any) => {
             console.log(`event_server_to_desktop: ${data}`);
@@ -57,6 +56,10 @@ const connectSocket = (protocol: string, host: string, token: string ): void => 
         });
         socket.on("event_mobile_to_desktop", (data: any) => {
             console.log(`event_mobile_to_desktop: ${data}`);
+            if (data.name === "connection_success" ) {
+                console.log("event_mobile_to_desktop > connection_success,  time to dismiss qr code");
+                qrWin.close();
+            }
             // s.emit("event_to_client", { hello: "world" });
         });
     });
@@ -65,9 +68,10 @@ const connectSocket = (protocol: string, host: string, token: string ): void => 
 module.exports = {
 
     createNewConnection: () => {
-        axios.post(connectionUrl()).then((response: AxiosResponse) => {
+        const pjson = require("../package.json");
+        axios.post(connectionUrl(), { version: pjson.version }).then((response: AxiosResponse) => {
             console.log(response.data);
-            connectSocket(response.data.protocol, response.data.host, response.data.token);
+            connectSocket(response.data.scheme, response.data.host, response.data.version , response.data.token);
         });
     },
 
@@ -88,15 +92,17 @@ module.exports = {
     openUrl: (url: string) => {
         console.log(`openUrl: ${url}`);
         const msgs = [{
-                    data: url ,
-                    name: "open_url",
-                }];
+            dataNumber: 0,
+            dataString: url ,
+            name: "open_url",
+        }];
         socket.emit("event_desktop_to_mobile", { messages: msgs } );
     },
     scroll: () => {
         console.log(`scroll`);
         const msgs = [{
-            data: "0",
+            dataNumber: 0,
+            dataString: "" ,
             name: "scroll",
         }];
         socket.emit("event_desktop_to_mobile", { messages: msgs } );
